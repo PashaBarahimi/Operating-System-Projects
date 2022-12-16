78040dd34f217b1f7661becc7471f6ce0a9db4a8
Pasha Barahimi <pashabarahimi@gmail.com>
2022-12-16 20:53:47 +0330

    Add indentation for namespace

--- CA3 - MultiThread Image Processing/serial/src/bmp24.cpp
+++ CA3 - MultiThread Image Processing/serial/src/bmp24.cpp
@@ -2,50 +2,23 @@
 
 namespace img
 {
-BMP24::BMP24(const std::string& filename)
-{
-    std::ifstream file(filename, std::ios::binary);
-    if (!file.is_open())
-        throw std::runtime_error("Could not open file " + filename);
-    load(file);
-}
-
-BMP24::BMP24(std::ifstream& file)
-{
-    if (!file.is_open())
-        throw std::runtime_error("File is not open");
-    load(file);
-}
-
-BMP24::BMP24(const BMP24& other)
-{
-    width_ = other.width_;
-    height_ = other.height_;
-    header_ = other.header_;
-    infoHeader_ = other.infoHeader_;
-    data_ = new Pixel[width_ * height_];
-    std::copy(other.data_, other.data_ + width_ * height_, data_);
-}
-
-BMP24::BMP24(int width, int height)
-{
-    width_ = width;
-    height_ = height;
-    data_ = new Pixel[width_ * height_];
-    std::fill(data_, data_ + width_ * height_, Pixel{});
-    setHeader();
-}
-
-BMP24::~BMP24()
-{
-    delete[] data_;
-}
-
-BMP24 BMP24::operator=(const BMP24& other)
-{
-    if (this != &other)
+    BMP24::BMP24(const std::string& filename)
     {
-        delete[] data_;
+        std::ifstream file(filename, std::ios::binary);
+        if (!file.is_open())
+            throw std::runtime_error("Could not open file " + filename);
+        load(file);
+    }
+
+    BMP24::BMP24(std::ifstream& file)
+    {
+        if (!file.is_open())
+            throw std::runtime_error("File is not open");
+        load(file);
+    }
+
+    BMP24::BMP24(const BMP24& other)
+    {
         width_ = other.width_;
         height_ = other.height_;
         header_ = other.header_;
@@ -53,92 +26,119 @@ BMP24 BMP24::operator=(const BMP24& other)
         data_ = new Pixel[width_ * height_];
         std::copy(other.data_, other.data_ + width_ * height_, data_);
     }
-    return *this;
-}
 
-void BMP24::load(std::ifstream& file)
-{
-    file.read(reinterpret_cast<char*>(&header_), sizeof(BMPHeader));
-    if (header_.signature != BMP_SIGNATURE)
-        throw std::runtime_error("Invalid BMP file");
-    file.read(reinterpret_cast<char*>(&infoHeader_), sizeof(BMPInfoHeader));
-    if (infoHeader_.bitsPerPixel != 24)
-        throw std::runtime_error("Only 24-bit BMP files are supported");
-    width_ = infoHeader_.width;
-    height_ = infoHeader_.height;
-    loadData(file);
-}
-
-void BMP24::loadData(std::ifstream& file)
-{
-    data_ = new Pixel[width_ * height_];
-    file.seekg(header_.dataOffset);
-    int padding = (4 - (width_ * sizeof(Pixel)) % 4) % 4;
-    for (int y = height_ - 1; y >= 0; --y)
+    BMP24::BMP24(int width, int height)
     {
-        file.read(reinterpret_cast<char*>(data_ + y * width_), width_ * sizeof(Pixel));
-        file.seekg(padding, std::ios::cur);
+        width_ = width;
+        height_ = height;
+        data_ = new Pixel[width_ * height_];
+        std::fill(data_, data_ + width_ * height_, Pixel { });
+        setHeader();
     }
-}
 
-void BMP24::save(const std::string& filename)
-{
-    std::ofstream file(filename, std::ios::binary);
-    if (!file.is_open())
-        throw std::runtime_error("Could not open file " + filename);
-    save(file);
-}
-
-void BMP24::save(std::ofstream& file)
-{
-    if (!file.is_open())
-        throw std::runtime_error("File is not open");
-    file.write(reinterpret_cast<char*>(&header_), sizeof(BMPHeader));
-    file.write(reinterpret_cast<char*>(&infoHeader_), sizeof(BMPInfoHeader));
-    int padding = (4 - (width_ * sizeof(Pixel)) % 4) % 4;
-    for (int y = height_ - 1; y >= 0; --y)
+    BMP24::~BMP24()
     {
-        file.write(reinterpret_cast<char*>(data_ + y * width_), width_ * sizeof(Pixel));
-        file.seekp(padding, std::ios::cur);
+        delete[] data_;
     }
-}
 
-void BMP24::setHeader()
-{
-    header_.signature = BMP_SIGNATURE;
-    header_.reserved = 0;
-    header_.dataOffset = sizeof(BMPHeader) + sizeof(BMPInfoHeader);
-    infoHeader_.headerSize = sizeof(BMPInfoHeader);
-    infoHeader_.width = width_;
-    infoHeader_.height = height_;
-    infoHeader_.planes = 1;
-    infoHeader_.bitsPerPixel = 24;
-    infoHeader_.compression = 0;
-    infoHeader_.imageSize = 0;
-    infoHeader_.xPixelsPerMeter = 0;
-    infoHeader_.yPixelsPerMeter = 0;
-    infoHeader_.colorsUsed = 0;
-    infoHeader_.colorsImportant = 0;
-}
+    BMP24 BMP24::operator=(const BMP24& other)
+    {
+        if (this != &other)
+        {
+            delete[] data_;
+            width_ = other.width_;
+            height_ = other.height_;
+            header_ = other.header_;
+            infoHeader_ = other.infoHeader_;
+            data_ = new Pixel[width_ * height_];
+            std::copy(other.data_, other.data_ + width_ * height_, data_);
+        }
+        return *this;
+    }
 
-Pixel& BMP24::operator()(int r, int c)
-{
-    return data_[r * width_ + c];
-}
+    void BMP24::load(std::ifstream& file)
+    {
+        file.read(reinterpret_cast<char*>(&header_), sizeof(BMPHeader));
+        if (header_.signature != BMP_SIGNATURE)
+            throw std::runtime_error("Invalid BMP file");
+        file.read(reinterpret_cast<char*>(&infoHeader_), sizeof(BMPInfoHeader));
+        if (infoHeader_.bitsPerPixel != 24)
+            throw std::runtime_error("Only 24-bit BMP files are supported");
+        width_ = infoHeader_.width;
+        height_ = infoHeader_.height;
+        loadData(file);
+    }
 
-Pixel BMP24::operator()(int r, int c) const
-{
-    return data_[r * width_ + c];
-}
+    void BMP24::loadData(std::ifstream& file)
+    {
+        data_ = new Pixel[width_ * height_];
+        file.seekg(header_.dataOffset);
+        int padding = (4 - (width_ * sizeof(Pixel)) % 4) % 4;
+        for (int y = height_ - 1; y >= 0; --y)
+        {
+            file.read(reinterpret_cast<char*>(data_ + y * width_), width_ * sizeof(Pixel));
+            file.seekg(padding, std::ios::cur);
+        }
+    }
 
-int BMP24::width() const
-{
-    return width_;
-}
+    void BMP24::save(const std::string& filename)
+    {
+        std::ofstream file(filename, std::ios::binary);
+        if (!file.is_open())
+            throw std::runtime_error("Could not open file " + filename);
+        save(file);
+    }
 
-int BMP24::height() const
-{
-    return height_;
-}
+    void BMP24::save(std::ofstream& file)
+    {
+        if (!file.is_open())
+            throw std::runtime_error("File is not open");
+        file.write(reinterpret_cast<char*>(&header_), sizeof(BMPHeader));
+        file.write(reinterpret_cast<char*>(&infoHeader_), sizeof(BMPInfoHeader));
+        int padding = (4 - (width_ * sizeof(Pixel)) % 4) % 4;
+        for (int y = height_ - 1; y >= 0; --y)
+        {
+            file.write(reinterpret_cast<char*>(data_ + y * width_), width_ * sizeof(Pixel));
+            file.seekp(padding, std::ios::cur);
+        }
+    }
+
+    void BMP24::setHeader()
+    {
+        header_.signature = BMP_SIGNATURE;
+        header_.reserved = 0;
+        header_.dataOffset = sizeof(BMPHeader) + sizeof(BMPInfoHeader);
+        infoHeader_.headerSize = sizeof(BMPInfoHeader);
+        infoHeader_.width = width_;
+        infoHeader_.height = height_;
+        infoHeader_.planes = 1;
+        infoHeader_.bitsPerPixel = 24;
+        infoHeader_.compression = 0;
+        infoHeader_.imageSize = 0;
+        infoHeader_.xPixelsPerMeter = 0;
+        infoHeader_.yPixelsPerMeter = 0;
+        infoHeader_.colorsUsed = 0;
+        infoHeader_.colorsImportant = 0;
+    }
+
+    Pixel& BMP24::operator()(int r, int c)
+    {
+        return data_[r * width_ + c];
+    }
+
+    Pixel BMP24::operator()(int r, int c) const
+    {
+        return data_[r * width_ + c];
+    }
+
+    int BMP24::width() const
+    {
+        return width_;
+    }
+
+    int BMP24::height() const
+    {
+        return height_;
+    }
 
 } // namespace img
--- CA3 - MultiThread Image Processing/serial/src/include/bmp24.hpp
+++ CA3 - MultiThread Image Processing/serial/src/include/bmp24.hpp
@@ -7,73 +7,73 @@
 
 namespace img
 {
-constexpr uint16_t BMP_SIGNATURE = 0x4D42; // "BM"
+    constexpr uint16_t BMP_SIGNATURE = 0x4D42; // "BM"
 
-struct Pixel
-{
-    enum Channel
+    struct Pixel
     {
-        RED,
-        GREEN,
-        BLUE
+        enum Channel
+        {
+            RED,
+            GREEN,
+            BLUE
+        };
+        uint8_t blue;
+        uint8_t green;
+        uint8_t red;
     };
-    uint8_t blue;
-    uint8_t green;
-    uint8_t red;
-};
 
 
 #pragma pack(push, 1)
-struct BMPHeader
-{
-    uint16_t signature;  // "BM"
-    uint32_t fileSize;   // size of the BMP file in bytes
-    uint32_t reserved;   // contains two 16-bit unused values
-    uint32_t dataOffset; // offset from the beginning of the file to the bitmap data
-};
-struct BMPInfoHeader
-{
-    uint32_t headerSize;      // size of this header in bytes (40)
-    int32_t  width;           // width of the bitmap in pixels
-    int32_t  height;          // height of the bitmap in pixels
-    uint16_t planes;          // number of color planes (must be 1)
-    uint16_t bitsPerPixel;    // number of bits per pixel (must be 24)
-    uint32_t compression;     // compression method being used
-    uint32_t imageSize;       // size of the raw bitmap data in bytes
-    int32_t  xPixelsPerMeter; // horizontal resolution of the image in pixels per meter
-    int32_t  yPixelsPerMeter; // vertical resolution of the image in pixels per meter
-    uint32_t colorsUsed;      // number of colors in the color palette
-    uint32_t colorsImportant; // number of important colors used
-};
+    struct BMPHeader
+    {
+        uint16_t signature;  // "BM"
+        uint32_t fileSize;   // size of the BMP file in bytes
+        uint32_t reserved;   // contains two 16-bit unused values
+        uint32_t dataOffset; // offset from the beginning of the file to the bitmap data
+    };
+    struct BMPInfoHeader
+    {
+        uint32_t headerSize;      // size of this header in bytes (40)
+        int32_t  width;           // width of the bitmap in pixels
+        int32_t  height;          // height of the bitmap in pixels
+        uint16_t planes;          // number of color planes (must be 1)
+        uint16_t bitsPerPixel;    // number of bits per pixel (must be 24)
+        uint32_t compression;     // compression method being used
+        uint32_t imageSize;       // size of the raw bitmap data in bytes
+        int32_t  xPixelsPerMeter; // horizontal resolution of the image in pixels per meter
+        int32_t  yPixelsPerMeter; // vertical resolution of the image in pixels per meter
+        uint32_t colorsUsed;      // number of colors in the color palette
+        uint32_t colorsImportant; // number of important colors used
+    };
 #pragma pack(pop)
 
-class BMP24
-{
-public:
-    BMP24(const std::string& filename);
-    BMP24(std::ifstream& file);
-    BMP24(const BMP24& other);
-    BMP24(int width, int height);
-    ~BMP24();
-    BMP24 operator=(const BMP24& other);
-    void save(const std::string& filename);
-    void save(std::ofstream& file);
-    Pixel& operator()(int r, int c);
-    Pixel operator()(int r, int c) const;
-    int width() const;
-    int height() const;
+    class BMP24
+    {
+    public:
+        BMP24(const std::string& filename);
+        BMP24(std::ifstream& file);
+        BMP24(const BMP24& other);
+        BMP24(int width, int height);
+        ~BMP24();
+        BMP24 operator=(const BMP24& other);
+        void save(const std::string& filename);
+        void save(std::ofstream& file);
+        Pixel& operator()(int r, int c);
+        Pixel operator()(int r, int c) const;
+        int width() const;
+        int height() const;
 
-private:
-    void load(std::ifstream& file);
-    void loadData(std::ifstream& file);
-    void write(std::ofstream& file);
-    void setHeader();
-    int width_;
-    int height_;
-    Pixel* data_;
-    BMPHeader header_;
-    BMPInfoHeader infoHeader_;
-};
+    private:
+        void load(std::ifstream& file);
+        void loadData(std::ifstream& file);
+        void write(std::ofstream& file);
+        void setHeader();
+        int width_;
+        int height_;
+        Pixel* data_;
+        BMPHeader header_;
+        BMPInfoHeader infoHeader_;
+    };
 
 } // namespace img
 
