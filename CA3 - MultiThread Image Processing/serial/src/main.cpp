dac291623b45becca3948c5b67686cdcce42efbb
Pasha Barahimi <pashabarahimi@gmail.com>
2022-12-20 15:38:26 +0330

    Add partial times for serial part

--- CA3 - MultiThread Image Processing/serial/src/main.cpp
+++ CA3 - MultiThread Image Processing/serial/src/main.cpp
@@ -7,6 +7,17 @@
 int TEST_ITERATIONS = 1;
 const std::string OUTPUT_FILE = "output.bmp";
 
+void executionTime(
+        std::chrono::time_point<std::chrono::high_resolution_clock>& start,
+        const std::string& name, bool fromStart = false, int iterations = 1)
+{
+    static auto s = start;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - (fromStart ? s : start);
+    std::cout << name << ": " << elapsed.count() / iterations << " ms" << std::endl;
+    start = end;
+}
+
 int main(int argc, char* argv[])
 {
     if (argc < 2)
@@ -24,13 +35,17 @@ int main(int argc, char* argv[])
         for (int i = 0; i < TEST_ITERATIONS; ++i)
         {
             img::BMP24 bmp(argv[1]);
+            // executionTime(start, "File Load");
             img::filter::flipHorizontal(bmp);
+            // executionTime(start, "Flip Horizontal");
             img::filter::emboss(bmp);
+            // executionTime(start, "Emboss");
             img::filter::diamond(bmp);
+            // executionTime(start, "Diamond");
             bmp.save(OUTPUT_FILE);
+            // executionTime(start, "File Save");
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        std::cout << "Execution Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / TEST_ITERATIONS << "ms" << std::endl;
+        executionTime(start, "Execution Time", true, TEST_ITERATIONS);
         return 0;
     }
     catch (const std::exception& e)
